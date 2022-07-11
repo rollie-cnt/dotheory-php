1c565fbfda58bedaa099702c2f57f788c4a98728
rollie-cnt <rollie.roy.llorca@codeandtheory.com>
2022-07-11 12:31:02 +0800

    php-401

---
+++ php-401/index.php
@@ -0,0 +1,6 @@
+<?php
+
+echo strip_tags('<p>In Italy<i>pasta</i> is commonly <br />
+eaten during <b>Lunch and Dinner</b> to have a lighter stomach.<br />
+The only thing that is common for Italians is that <i>coffee</i> is always served 3 or 4 times a day.</p>
+<script>alert("Cafe!");</script>', '<p><i>');
